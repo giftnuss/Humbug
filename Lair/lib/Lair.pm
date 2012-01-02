07c317fd975b61ac907fd6e122ddc07ec7a341c7
Sebatian B. Knapp <rock@ccls-online.de>
2012-01-03 00:07:51 +0100

    start of a hopefully smooth project

---
+++ Lair/lib/Lair.pm
@@ -0,0 +1,18 @@
+  package Lair;
+# *************
+use Badger::Class;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Lair - a small web framework
+
+=head2 DESCRIPTION
+
+A try to port Layland from Moose to Badger
+framework. This will be a friendly fork, only
+with intention to see how far it goes.
+
